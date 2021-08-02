6b85afd77b5374e55ce3ebbd28555b87de63c163
Godwin Nwachukwu <nwgwin@gmail.com>
2021-08-02 08:52:55 +0000

    improve code to display message for unfavorited movies

--- src/FavoriteMovieItem.js
+++ src/FavoriteMovieItem.js
@@ -5,8 +5,17 @@ class FavoriteMovieItem extends Component {
     
     const movieTitle = this.props.movieName;
     const favoritedList = this.props.usersMoviesList;
-    let favoritedListArr =  favoritedList.filter(favoritedMovieItem => movieTitle === favoritedMovieItem[1]);
+    
+    let favoritedListArr = favoritedList.filter(favoritedMovieItem => movieTitle === favoritedMovieItem[1]);
     console.log(movieTitle);
+    console.log(favoritedListArr);
+    console.log(favoritedListArr.length);
+
+    if (favoritedListArr.length === 0){
+      favoritedListArr.push([<p>None of the current users liked this movie</p>]);
+    }
+    
+
     console.log(favoritedListArr);
     
    

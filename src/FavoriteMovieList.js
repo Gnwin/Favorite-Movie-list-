4e1c39d59751c36c2dc3c87d121a4f4d6e288457
Godwin Nwachukwu <nwgwin@gmail.com>
2021-08-03 17:39:15 +0000

    remove commented code and correct indentation

--- src/FavoriteMovieList.js
+++ src/FavoriteMovieList.js
@@ -24,24 +24,7 @@ class FavoriteMovieList extends Component {
     )
   }
 }
-    
-      
-  	//return (
-      //<ol>
-      //{user_movie_ids.map((list) => (
-    	//<li key = {list[0]}>
-		  //{users[list[0]].name}'s favorite movie is {movies[list[1]].name} 
-      	//</li>
-      //))}
-      //</ol>
-    //)
-      
-      //<ol>
-      //{profiles.map((profile) => (
-    	//<li key = {profile.id}>
-		  //{users[profile.userID].name}'s favorite movie is {movies[profile.favoriteMovieID].name} 
-      	//</li>
-      //))}
-      //</ol>
+
+
 
 export default FavoriteMovieList
